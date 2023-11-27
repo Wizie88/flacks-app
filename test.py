c46e1d751f69fbe2945c0dec1b4d4f7098591b01
wizie88 <uyieric@ymail.com>
2023-11-26 23:10:01 -0500

    update

--- test.py
+++ test.py
@@ -1,12 +1,24 @@
 import unittest
-from app import app
-class TestHello(unittest.TestCase):
+from flask import Flask, render_template
+from app import app  # Assuming your Flask app is in a file named app.py
+
+class TestFlaskApp(unittest.TestCase):
+
     def setUp(self):
-        app.testing = True
         self.app = app.test_client()
-    def test_hello(self):
-        rv = self.app.get('/')
-        self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'I am almost a DevOps Engineer!\n')
-if __name__ == '__main__':
+
+    def test_hello_route(self):
+        response = self.app.get('/')
+        self.assertIn(b"I'm almost a DevOps engineer", response.data)
+
+    def test_static_file_exists(self):
+        response = self.app.get('/static/images/sample.jpeg')
+        self.assertEqual(response.status_code, 200)
+
+    def test_404_error(self):
+        response = self.app.get('/nonexistent')
+        self.assertEqual(response.status_code, 404)
+        self.assertIn(b"Page not found", response.data)
+
+if _name_ == '_main_':
     unittest.main()
