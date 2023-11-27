0d8d4d901d55eaa41e1cc4c79c9bc433475af7f4
wizie88 <uyieric@ymail.com>
2023-11-26 22:47:00 -0500

    update

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'I am almost a DevOps Engineer!\n'
+render_template('index.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
