6f40ad08e1c73903eacd4126f8c95b21dd450410
wizie88 <uyieric@ymail.com>
2023-11-27 01:11:50 -0500

    u

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
-from flask import Flask, render_template, send_from_directory
-import os
-
+from flask import Flask
 app = Flask(__name__)
-
 @app.route('/')
 def hello():
-    try:
-        content = "I'm almost a DevOps engineer"
-        print(f"Returning content: {content}")
-        return render_template('index.html')
-    except Exception as e:
-        print(f"Error: {e}")
-        raise
-
+    return 'I am almost a DevOps Engineer!\n'
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
--- test.py
+++ test.py
@@ -1,33 +1,12 @@
 import unittest
-from flask import Flask, render_template, send_from_directory
 from app import app
-
-class TestFlaskApp(unittest.TestCase):
-
+class TestHello(unittest.TestCase):
     def setUp(self):
+        app.testing = True
         self.app = app.test_client()
-        self.app_context = app.app_context()
-        self.app_context.push()
-
-    def tearDown(self):
-        self.app_context.pop()
-
-    def test_hello_route(self):
-        response = self.app.get('/')
-        print(f"Response status code: {response.status_code}")
-        print(f"Response data: {response.data}")
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b"I'm almost a DevOps engineer", response.data)
-
-    def test_static_file_exists(self):
-        response = self.app.get('/static/images/sample.jpeg')
-        self.assertEqual(response.status_code, 200)
-
-    def test_404_error(self):
-        response = self.app.get('/nonexistent')
-        self.assertEqual(response.status_code, 404)
-        self.assertIn(b'Not Found', response.data)
-        self.assertIn(b'The requested URL was not found on the server.', response.data)
-
+    def test_hello(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'I am almost a DevOps Engineer!\n')
 if __name__ == '__main__':
     unittest.main()
