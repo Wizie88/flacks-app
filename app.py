6668ceaab76b1960d1a1bc35ec87fa80be3b859d
wizie88 <uyieric@ymail.com>
2023-11-27 00:28:02 -0500

    u

--- app.py
+++ app.py
@@ -9,10 +9,6 @@ def hello():
     print(f"Returning content: {content}")
     return render_template('index.html')
 
-@app.route('/static/<path:filename>')
-def serve_static(filename):
-    root_dir = os.path.dirname(os.getcwd())
-    return send_from_directory(os.path.join(root_dir, 'static'), filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
